332f70710276d19f298829c5a120ceeabe2b0423
GhostMonarch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elijahpaul@Elijahs-MacBook-Air ~ % git config --global user.email ghostmonarchalerts@gmail.comgit config --global user.name GhostMonarch <ghostmonarchalets@gmaile.com>
2026-03-05 23:35:24 -0700

    Remove social icons from footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,8 @@
 import { motion } from 'motion/react';
-import { Instagram, Youtube, Twitter, Tv, Link } from 'lucide-react';
+
 import { EditableText } from './EditableText';
 
-const socials = [
-  { icon: Youtube, href: 'https://www.youtube.com/@itsashleighxoxo', label: 'YouTube' },
-  { icon: Instagram, href: 'https://www.instagram.com/itsashleighxoxo/', label: 'Instagram' },
-  { icon: Link, href: 'https://link.me/itsashleighxoxo', label: 'Link.me' },
-  { icon: Tv, href: 'https://www.twitch.tv/Pintsizexo', label: 'Twitch' },
-  { icon: Twitter, href: 'https://x.com/itsashleighxxoo', label: 'X' },
-];
+
 
 export function Footer() {
   return (
@@ -26,26 +20,7 @@ export function Footer() {
             <EditableText contentKey="footer.logo" as="span" />
           </motion.div>
 
-          {/* Social Icons */}
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            className="flex items-center gap-5"
-          >
-            {socials.map((s) => (
-              <a
-                key={s.label}
-                href={s.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                title={s.label}
-                className="w-10 h-10 border border-white/20 flex items-center justify-center text-white/60 hover:text-white hover:border-white/60 transition-all"
-              >
-                <s.icon className="w-4 h-4" />
-              </a>
-            ))}
-          </motion.div>
+
 
           {/* Copyright */}
           <motion.div
